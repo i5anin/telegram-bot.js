6a2a314eb5a26d9fa573fa99052a60348460e789
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-08-29 10:14:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,48 @@
 require("dotenv").config();
-const { Telegraf } = require("telegraf");
-const axios = require("axios");
+// const axios = require("axios");
+
+const { Telegraf, Markup } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply("send me ur geolocation"));
+// Обработка команды /start
+bot.command("start", (ctx) => {
+  // Создание inline-клавиатуры
+  const inlineMessageKeyboard = Markup.inlineKeyboard([
+    [
+      Markup.button.callback("1", "1"),
+      Markup.button.callback("2", "2"),
+      Markup.button.callback("3", "3"),
+    ],
+    [
+      Markup.button.callback("4", "4"),
+      Markup.button.callback("5", "5"),
+      Markup.button.callback("6", "6"),
+    ],
+    [
+      Markup.button.callback("7", "7"),
+      Markup.button.callback("8", "8"),
+      Markup.button.callback("9", "9"),
+    ],
+    [Markup.button.callback("<", "prev"), Markup.button.callback(">", "next")],
+  ]);
 
-bot.on("message", async (ctx) => {
-  console.log(ctx.message);
-  if (ctx.message.location) {
-    const weatherAPIUrl = `https://openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=${process.env.OWEATHER_APIKEY}`;
-    const response = await axios.get(weatherAPIUrl);
-    ctx.reply(
-      `${response.data.name}: ${response.data.weather[0].main} ${response.data.main.temp} °C`
-    );
+  // Отправка сообщения с inline-клавиатурой
+  ctx.reply("Пожалуйста, выберите дату", inlineMessageKeyboard);
+});
+
+// Обработчик кнопок
+bot.action(/.+/, async (ctx) => {
+  const data = ctx.match[0];
+  if (data === "next") {
+    ctx.reply("Обработка кнопки next"); // Обработка кнопки "next"
+    await ctx.answerCbQuery();
+  } else if (data === "prev") {
+    ctx.reply("Обработка кнопки prev"); // Обработка кнопки "prev"
+    await ctx.answerCbQuery();
+  } else {
+    ctx.reply(`Вы выбрали ${data}`); // Обработка числовых кнопок
+    await ctx.answerCbQuery(`You have selected ${data}`);
   }
 });
 
