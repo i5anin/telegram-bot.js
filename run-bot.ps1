c0dae3690f3ebe4a1fe050686c33459cac2de6d2
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-01 13:27:37 +0300

    Create run-bot.ps1

---
+++ run-bot.ps1
@@ -0,0 +1,8 @@
+# Clear the console screen
+Clear-Host
+
+# Change the current directory to the directory where the script is located
+Set-Location $PSScriptRoot
+
+# Run the Node.js script
+node bot.js
