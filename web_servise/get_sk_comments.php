59257bb288a7598e87ec3bdd5b4a1518158210f2
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-06 14:03:10 +0300

    Update get_sk_comments.php

--- web_servise/get_sk_comments.php
+++ web_servise/get_sk_comments.php
@@ -12,7 +12,7 @@ function get_sk_comments()
     $mysqli = mysqli_connect($server, $user, $pass, $db);
     mysqli_set_charset($mysqli, 'utf8mb4');
 
-    $stmt = mysqli_prepare($mysqli, "SELECT `id`,`user_id`, `date`,  `name`, `description` FROM `sk_comment` WHERE `completed` = 0");
+    $stmt = mysqli_prepare($mysqli, "SELECT `id_task`,`user_id`, `date`,  `name`, `description` FROM `sk_comment` WHERE `completed` = 0");
 
     if (!mysqli_stmt_execute($stmt)) {
         return json_encode(['error' => 'Ошибка: ' . mysqli_stmt_error($stmt)]);
@@ -33,8 +33,10 @@ function get_sk_comments()
 
 $comments = get_sk_comments();
 
-if ($comments) {
+if (isset($comments['error'])) {
+    echo json_encode(['error' => $comments['error']]);
+} elseif (!empty($comments)) {
     echo json_encode(['comments' => $comments]);
 } else {
-    echo json_encode(['error' => 'ОШИБКА!']);
+    echo json_encode(['error' => 'Комментарии не найдены']);
 }
