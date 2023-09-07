251dbb4e7984908f614d49a91489e274d392e10b
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-07 14:33:02 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    apps: [
-      {
-        name: 'my-app',
-        script: 'app.js',
-        env: {
-          NODE_ENV: 'development',
-        },
-        env_development: {
-          NODE_ENV: 'development',
-        },
+  apps: [
+    {
+      name: 'my-bot', // Уникальное имя для бота
+      script: 'bot.js',
+      env: {
+        NODE_ENV: 'development',
       },
-    ],
-  };
-  
+      env_development: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
+};
