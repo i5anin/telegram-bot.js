230eb8710548571548559e9596ccb3460810e6e5
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-11 09:23:41 +0300

    Update get_id_comment.php

--- web_servise/get_id_comment.php
+++ web_servise/get_id_comment.php
@@ -1,44 +1,62 @@
 <?php
-function get_sk_comment_by_id($id_task)
-{
-    $dbConfig = require 'sql_config.php';
+// Подключаем конфигурационный файл
+$dbConfig = require 'sql_config.php';
 
-    $server = $dbConfig['server'];
-    $user = $dbConfig['user'];
-    $pass = $dbConfig['pass'];
-    $db = $dbConfig['db'];
+// Получаем параметры для подключения к БД
+$server = $dbConfig['server'];
+$user = $dbConfig['user'];
+$pass = $dbConfig['pass'];
+$db = $dbConfig['db'];
 
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $mysqli = mysqli_connect($server, $user, $pass, $db);
-    mysqli_set_charset($mysqli, 'utf8mb4');
+// Подключаемся к БД
+$mysqli = new mysqli($server, $user, $pass, $db);
 
-    $stmt = mysqli_prepare($mysqli, "SELECT `id_task`, `user_id`, `date`, `name`, `description` FROM `sk_comment` WHERE `id_task` = ? AND `completed` = 0");
-    mysqli_stmt_bind_param($stmt, 'i', $id_task);
-
-    if (!mysqli_stmt_execute($stmt)) {
-        return json_encode(['error' => 'Ошибка: ' . mysqli_stmt_error($stmt)]);
-    }
-
-    $result = mysqli_stmt_get_result($stmt);
-    $comment = mysqli_fetch_assoc($result);
-
-    mysqli_stmt_close($stmt);
-    mysqli_close($mysqli);
-
-    return $comment;
+// Проверяем успешность подключения
+if ($mysqli->connect_error) {
+    die(json_encode(['error' => "Connection failed: " . $mysqli->connect_error]));
 }
 
-if (isset($_GET['id_task'])) {
-    $id_task = intval($_GET['id_task']);
-    $comment = get_sk_comment_by_id($id_task);
+// Устанавливаем кодировку
+$mysqli->set_charset('utf8mb4');
 
-    if (isset($comment['error'])) {
-        echo json_encode(['error' => $comment['error']]);
-    } elseif (!empty($comment)) {
-        echo json_encode(['comment' => $comment]);
+// Подготавливаем запрос
+if ($stmt = $mysqli->prepare("SELECT `id_task`, `user_id`, `date`, `name`, `description` FROM `sk_comment` WHERE `user_id` = ? AND `completed` = 0")) {
+
+    // Привязываем параметры
+    $stmt->bind_param("i", $_GET['user_id']);
+
+    // Выполняем запрос
+    $stmt->execute();
+
+    // Привязываем результаты к переменным
+    $stmt->bind_result($id_task, $user_id, $date, $name, $description);
+
+    // Инициализируем массив для хранения результатов
+    $comments = [];
+
+    // Получаем и сохраняем все строки результата
+    while ($stmt->fetch()) {
+        $comments[] = [
+            'id_task' => $id_task,
+            'user_id' => $user_id,
+            'date' => $date,
+            'name' => $name,
+            'description' => $description,
+        ];
+    }
+
+    // Закрываем запрос
+    $stmt->close();
+
+    // Проверяем, нашлись ли какие-то результаты
+    if (!empty($comments)) {
+        echo json_encode(['comments' => $comments]);
     } else {
-        echo json_encode(['error' => 'Comment not found']);
+        echo json_encode(['error' => 'Comments not found', 'debug' => ['GET' => $_GET]]);
     }
 } else {
-    echo json_encode(['error' => 'ID not provided']);
+    echo json_encode(['error' => 'User ID not provided', 'debug' => ['GET' => $_GET]]);
 }
+
+// Закрываем подключение к БД
+$mysqli->close();
