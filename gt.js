6781cd44c8ce9ceb791278bac20cc408a5657764
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-13 14:15:01 +0300

    Update gt.js

--- gt.js
+++ gt.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const fs = require('fs')
 const { Telegraf } = require('telegraf')
 const axios = require('axios')
 const { BOT_TOKEN_ORIG } = process.env
@@ -42,7 +43,44 @@ const fetchGroupTitle = async (groupId) => {
     }
 }
 
+const excludedUserIds = [
+    '465727872', //Роман Гусаров
+    '6387629342', // ПФ-Форум bot
+]
+
+const checkAdmins = (admins, groupName, userIdsFromApi, logStream) => {
+    for (const admin of admins) {
+        const userId = admin.user.id.toString()
+
+        // Пропустить, если это бот или пользователь в списке исключений
+        if (admin.user.is_bot || excludedUserIds.includes(userId)) {
+            continue
+        }
+
+        if (!userIdsFromApi.has(userId)) {
+            const fio =
+                admin.user.first_name + ' ' + (admin.user.last_name || '')
+            const customTitle = admin.custom_title
+                ? admin.custom_title
+                : 'Нет метки'
+            const username = admin.user.username
+                ? `@${admin.user.username}`
+                : 'Нет username'
+
+            console.log('-----')
+            console.log(`id ${userId}`)
+            console.log(`fio ${fio}`)
+            console.log(`username ${username}`)
+            const logMessage = `WARNING: админ с меткой "${customTitle}" из группы "${groupName}", но отсутствует в get_all_fio.php`
+            console.log(logMessage)
+            logStream.write(logMessage + '\n')
+            console.log('-----')
+        }
+    }
+}
+
 const compareGroups = async () => {
+    const logStream = fs.createWriteStream('group1_log.txt', { flags: 'a' })
     const [adminsGroup1, adminsGroup2, usersData, title1, title2] =
         await Promise.all([
             fetchAdmins(groupId1),
@@ -52,13 +90,45 @@ const compareGroups = async () => {
             fetchGroupTitle(groupId2),
         ])
 
+    const userIdsFromApi = new Set(
+        usersData
+            .map((user) => user.user_id.toString())
+            .filter((id) => !excludedUserIds.includes(id))
+    )
+
+    // Проверяем админов для каждой группы
+    checkAdmins(adminsGroup1, title1, userIdsFromApi, logStream)
+    checkAdmins(adminsGroup2, title2, userIdsFromApi, logStream)
+
+    logStream.end()
+
+    for (const admin of [...adminsGroup1, ...adminsGroup2]) {
+        const userId = admin.user.id.toString()
+
+        if (!userIdsFromApi.has(userId)) {
+            const fio =
+                admin.user.first_name + ' ' + (admin.user.last_name || '')
+            const customTitle = admin.custom_title
+                ? admin.custom_title
+                : 'Нет метки'
+            const username = admin.user.username
+                ? `@${admin.user.username}`
+                : 'Нет username'
+
+            console.log('-----')
+            console.log(`id ${userId}`)
+            console.log(`fio ${fio}`)
+            console.log(`username ${username}`)
+            console.log(
+                `WARNING: админ с меткой "${customTitle}", но отсутствует в get_all_fio.php`
+            )
+            console.log('-----')
+        }
+    }
+
     for (const userData of usersData) {
         const userId = userData.user_id.toString()
         const fio = userData.fio
-
-        console.log('-----')
-        console.log(`id ${userId}`)
-        console.log(`fio ${fio}`)
 
         const admin1 = adminsGroup1.find(
             (admin) => admin.user.id.toString() === userId
@@ -69,6 +139,20 @@ const compareGroups = async () => {
 
         const label1 = admin1 ? admin1.custom_title : 'Нет метки'
         const label2 = admin2 ? admin2.custom_title : 'Нет метки'
+
+        // Если пользователь есть в обеих группах и у него есть кастомные метки в обеих группах, пропускаем его
+        if (
+            admin1 &&
+            admin2 &&
+            label1 !== 'Нет метки' &&
+            label2 !== 'Нет метки'
+        ) {
+            continue
+        }
+
+        console.log('-----')
+        console.log(`id ${userId}`)
+        console.log(`fio ${fio}`)
 
         if (admin1) {
             console.log(`${title1} кастомная метка  ${label1}`)
