9dfbbc288a1ee5e93b2b737360a8d8461bbb7e80
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-14 15:05:41 +0300

    Update cron.js

--- src/modules/cron.js
+++ src/modules/cron.js
@@ -3,8 +3,8 @@ const cron = require('node-cron');
 const { notifyAllUsers } = require('#src/modules/notify')  // Уведомления пользователя
 
 module.exports = function initCronJobs() {
-    cron.schedule('*/20 * * * *', async () => {
-        // console.log('Running a task every 20 minutes');
+    cron.schedule('*/1 * * * *', async () => {
+        console.log('Running a task every 2 minutes');
         await notifyAllUsers();
     });
 };
