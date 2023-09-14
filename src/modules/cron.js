5902d8da006df98d83af5bf8655951ff873abe74
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-14 16:51:28 +0300

    Update cron.js

--- src/modules/cron.js
+++ src/modules/cron.js
@@ -1,61 +1,61 @@
 // cronJobs.js
-const cron = require('node-cron');
-const { Telegraf } = require('telegraf');
-const axios = require('axios');
+const cron = require('node-cron')
+const { Telegraf } = require('telegraf')
+const axios = require('axios')
 const { notifyAllUsers } = require('#src/modules/notify')  // Уведомления пользователя
 
- function initCronJobs() {
+function initCronJobs() {
     // Уведомлять каждые 2 минуты
     cron.schedule('*/2 * * * *', async () => {
-        console.log('Running a task every 2 minutes');
-        await notifyAllUsers();
-    });
+        console.log('Running a task every 2 minutes')
+        await notifyAllUsers()
+    })
 
     // Уведомлять каждый день в 7:30
     cron.schedule('30 7 * * *', async () => {
-        console.log('Running a task at 7:30 every day');
-        await morningNotification();
-    });
-};
+        console.log('Running a task at 7:30 every day')
+        await morningNotification()
+    })
+}
+
 async function morningNotification() {
     try {
-        const response = await axios.get(`${OPLATA_API}/get.php?key=${SECRET_KEY}`);
+        const response = await axios.get(`${OPLATA_API}/get.php?key=${SECRET_KEY}`)
         if (response.data && response.data.payments) {
-            const payments = response.data.payments;
-            console.log("Payments received:", payments);
+            const payments = response.data.payments
+            console.log('Payments received:', payments)
 
             // Формируем сообщение для администратора
-            let message = 'Утренний отчет по оплате:\n';
+            let message = 'Утренний отчет по оплате:\n'
             payments.forEach((payment) => {
                 // Форматирование суммы
-                const formattedSum = Number(payment.sum).toLocaleString('ru-RU');
+                const formattedSum = Number(payment.sum).toLocaleString('ru-RU')
 
                 // Форматирование даты
-                const [year, month, day] = payment.date.split('-');
-                const formattedDate = `${day}.${month}.${year}`;
+                const [year, month, day] = payment.date.split('-')
+                const formattedDate = `${day}.${month}.${year}`
 
-                message += `Дата: <code>${formattedDate}</code>\n`;
-                message += `Имя клиента: <code>${payment.client_name}</code>\n`;
-                message += `Сумма: <code>${formattedSum} руб</code>\n`;
-                message += `Информация: <code>${payment.info}</code>\n`;
-                message += '--------------------\n';
-            });
+                message += `Дата: <code>${formattedDate}</code>\n`
+                message += `Имя клиента: <code>${payment.client_name}</code>\n`
+                message += `Сумма: <code>${formattedSum} руб</code>\n`
+                message += `Информация: <code>${payment.info}</code>\n`
+                message += '--------------------\n'
+            })
 
             // Отправляем сообщение каждому администратору
-            const ADMIN_IDS = [GRAND_ADMIN , "6522743169"];
+            const ADMIN_IDS = [GRAND_ADMIN, LOG_CHANNEL_ID]
             for (const adminId of ADMIN_IDS) {
-                await bot.telegram.sendMessage(adminId, message, { parse_mode: 'HTML' });
+                await bot.telegram.sendMessage(adminId, message, { parse_mode: 'HTML' })
             }
         } else {
-            console.error("No payments found");
+            console.error('No payments found')
         }
     } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error)
     }
 }
 
 
-
-module.exports ={morningNotification,initCronJobs}
+module.exports = { morningNotification, initCronJobs }
 
 
