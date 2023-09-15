174b88f8fa9087c4c7609f7fd9503a590b5686c3
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-15 08:22:59 +0300

    Утренний отчет по оплате

--- src/modules/cron.js
+++ src/modules/cron.js
@@ -19,6 +19,7 @@ function initCronJobs() {
 }
 
 async function morningNotification() {
+    let e_ADMIN_IDS = null;
     try {
         const response = await axios.get(`${OPLATA_API}/get.php?key=${SECRET_KEY}`)
         if (response.data && response.data.payments) {
@@ -26,7 +27,7 @@ async function morningNotification() {
             console.log('Payments received:', payments)
 
             // Формируем сообщение для администратора
-            let message = 'Утренний отчет по оплате:\n'
+            let message = 'Отчет по оплате:\n'
             payments.forEach((payment) => {
                 // Форматирование суммы
                 const formattedSum = Number(payment.sum).toLocaleString('ru-RU')
@@ -43,8 +44,9 @@ async function morningNotification() {
             })
 
             // Отправляем сообщение каждому администратору
-            const ADMIN_IDS = [GRAND_ADMIN, LOG_CHANNEL_ID]
+            const ADMIN_IDS = [GRAND_ADMIN, LOG_CHANNEL_ID, '1107003647']
             for (const adminId of ADMIN_IDS) {
+                e_ADMIN_IDS = adminId
                 await bot.telegram.sendMessage(adminId, message, { parse_mode: 'HTML' })
             }
         } else {
@@ -54,9 +56,9 @@ async function morningNotification() {
         console.error('Error fetching data:', error)
         await bot.telegram.sendMessage(
             LOG_CHANNEL_ID,
-            `\n<code>${error}</code>`,
-            { parse_mode: 'HTML' }
-        );
+            `Попытка отправить сообщение <code>${e_ADMIN_IDS}</code> оплата \n<code>${error}</code>`,
+            { parse_mode: 'HTML' },
+        )
     }
 }
 
