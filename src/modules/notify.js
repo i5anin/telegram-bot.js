f81405436eca68b34acb8afae612c399820dd218
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-20 13:55:04 +0300

    Update notify.js

--- src/modules/notify.js
+++ src/modules/notify.js
@@ -3,19 +3,25 @@ const fetchData = require('#src/utils/helpers')
 const { fetchComments } = require('#src/modules/comment')
 const { sendToLog } = require('#src/utils/admin')
 
+
 // Функция для уведомления одного пользователя о некомментированных задачах
 async function notifyUsers(ctx) {
+    console.log('notifyUsers')
     const chatId = ctx.message.chat.id // Получаем ID чата из контекста сообщения
     await sendToLog(ctx)
     try {
         // Получаем список некомментированных задач для данного пользователя
         const uncommentedTasks = await fetchComments(chatId)
-
         // Проверяем, есть ли какие-либо некомментированные задачи
         if (!uncommentedTasks || uncommentedTasks.length === 0) {
-            const errorMessage = 'Пустые комментарии не найдены.';
-            //const errorMessage = uncommentedTasks ? 'Пустые комментарии не найдены.' : 'Произошла ошибка при получении комментариев.'
-            return bot.telegram.sendMessage(chatId, errorMessage, { parse_mode: 'HTML' })
+            // const errorMessage = 'Пустые комментарии не найдены.';
+            const errorMessage = uncommentedTasks ? 'Пустые комментарии не найдены.' : 'Пустые комментарии не найдены.'
+            try {
+                return bot.telegram.sendMessage(chatId, errorMessage, { parse_mode: 'HTML' })
+            } catch (error) {
+                console.log('Error sending message:', error)
+            }
+
         }
 
         // Фильтруем задачи, оставляем только те, которые принадлежат текущему пользователю
@@ -25,13 +31,26 @@ async function notifyUsers(ctx) {
         if (userActualComments.length === 0) return
 
         // Формируем сообщение для пользователя
-        const { id_task, kolvo_brak, det_name, date } = userActualComments[0]
+        const { id_task, kolvo_brak, det_name, date, specs_nom_id, type } = userActualComments[0]
+
+        const typeMapping = {
+            'ПО': 'Пооперационный контроль окончательный',
+            'ПН': 'Пооперационный контроль неокончательный',
+            'УО': 'Контроль перед упаковкой окончательный',
+            'УН': 'Контроль перед упаковкой неокончательный',
+        }
+
+        const typeString = typeMapping[type] || 'Неизвестный тип'
+
         const message = `<b>Пожалуйста, прокомментируйте следующую операцию:</b>`
             + `<code>(1/${userActualComments.length})</code>\n\n`
             + `<b>Название и обозначение:</b>\n<code>${det_name}</code>\n`
             + `<b>Брак:</b> <code>${kolvo_brak}</code>\n`
+            + `<b>Контроль:</b> <code>${typeString}</code>\n`
+            + `<b>Партия:</b> <code>${specs_nom_id}</code>\n`
             + `<b>Дата:</b> <code>${date}</code>\n\n`
-            + `task_ID: <code>${id_task}</code>`
+            + `task_ID: <code>${id_task}</code>\n`
+            + `<i>необходимо прокомментировать через "ответить" на это сообщение</i>`
         // Отправляем сообщение
         ctx.session.userComments = userActualComments[0]
         ctx.session.id_task = id_task
@@ -47,8 +66,8 @@ async function notifyUsers(ctx) {
         await bot.telegram.sendMessage(
             LOG_CHANNEL_ID,
             `\n<code>${error}</code>`,
-            { parse_mode: 'HTML' }
-        );
+            { parse_mode: 'HTML' },
+        )
     }
     ctx.session.isAwaitComment = true
 }
@@ -58,37 +77,38 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-let sentTaskIds = [];
+let sentTaskIds = []
 
 // Функция для уведомления всех пользователей
 async function notifyAllUsers(ctx) {
+
     // Добавлен ctx в качестве аргумента для доступа к сессии и другим методам контекста
 
     // Получаем все комментарии
-    const allComments = await fetchComments();
+    const allComments = await fetchComments()
 
     // Получаем данные всех пользователей
-    const data = await fetchData(`${WEB_API}/comment/get_all.php?key=${SECRET_KEY}`);
+    const data = await fetchData(`${WEB_API}/comment/get_all.php?key=${SECRET_KEY}`)
 
     // Извлекаем идентификаторы чатов из всех комментариев и удаляем дубликаты
-    const user_ids = [...new Set(data.comments.map(comment => comment.user_id))];
+    const user_ids = [...new Set(data.comments.map(comment => comment.user_id))]
 
     // Проходим по каждому идентификатору чата
     for (const chatId of user_ids) {
 
         // Фильтруем комментарии для данного пользователя
-        const userComments = allComments.filter(comment => comment.user_id === chatId);
+        const userComments = allComments.filter(comment => comment.user_id === chatId)
 
         // Если у пользователя нет комментариев, пропустить итерацию
-        if (userComments.length === 0) continue;
+        if (userComments.length === 0) continue
 
         // Извлекаем данные из первого комментария
-        const { id_task, kolvo_brak, det_name, date } = userComments[0];
+        const { id_task, kolvo_brak, det_name, date } = userComments[0]
 
         // Проверяем, отправлялось ли сообщение этому пользователю ранее по этой задаче
         if (sentTaskIds.includes(id_task)) {
-            console.log(`Сообщение для id_task ${id_task} уже отправлено на chatId: ${chatId}`);
-            continue;
+            console.log(`Сообщение для id_task ${id_task} уже отправлено на chatId: ${chatId}`)
+            continue
         }
 
         // Проверяем, отправлялось ли сообщение этому пользователю ранее по этой задаче
@@ -104,20 +124,20 @@ async function notifyAllUsers(ctx) {
             + `<code>${det_name}</code>\n`
             + `<b>Брак:</b> <code>${kolvo_brak}</code>\n`
             + `<b>Дата:</b> <code>${date}</code>\n\n`
-            + `<code>Cron</code> task_ID: <code>${id_task}</code>\n`;
+            + `<code>Cron</code> task_ID: <code>${id_task}</code>\n`
 
         // Пытаемся отправить сообщение
         try {
-            await bot.telegram.sendMessage(chatId, message, { parse_mode: 'HTML' });
-            console.log(`Cron Сообщение отправлено на chatId: ${chatId}`);
+            await bot.telegram.sendMessage(chatId, message, { parse_mode: 'HTML' })
+            console.log(`Cron Сообщение отправлено на chatId: ${chatId}`)
 
-            await sleep(2000); // Задержка на 5 секунд
+            await sleep(2000) // Задержка на 5 секунд
         } catch (error) { // Если возникает ошибка при отправке
-            console.error(`Failed to send message to chatId: ${chatId}`, error);
+            console.error(`Failed to send message to chatId: ${chatId}`, error)
             // Отправляем уведомление в канал логирования
-            await bot.telegram.sendMessage(LOG_CHANNEL_ID, `Не удалось отправить сообщение на chatId: ${chatId}\nError: ${error}`, { parse_mode: 'HTML' });
+            await bot.telegram.sendMessage(LOG_CHANNEL_ID, `Не удалось отправить сообщение на chatId: ${chatId}\nError: ${error}`, { parse_mode: 'HTML' })
         }
-        sentTaskIds.push(id_task);
+        sentTaskIds.push(id_task)
     }
 }
 
