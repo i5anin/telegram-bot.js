18320e9ec9de8724e58d0e77a78a13b22f45869e
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-25 16:11:03 +0300

    Update help.js

--- src/modules/help.js
+++ src/modules/help.js
@@ -5,59 +5,67 @@ const axios = require('axios')
 async function getUserInfo(userId) {
     try {
         // Запрашиваем данные всех пользователей
-        const response = await axios.get(`${WEB_API}/users/get_all_fio.php`);
+        const response = await axios.get(`${WEB_API}/users/get_all_fio.php`)
 
         // Ищем пользователя с заданным userId в полученных данных
-        const user = response.data.users_data.find(u => u.user_id === userId);
+        const user = response.data.users_data.find(u => u.user_id === userId)
 
         if (user) {
             // Если пользователь найден, возвращаем его данные
             return {
                 userId: user.user_id,
                 fio: user.fio,
-            };
+            }
         } else {
             // Если пользователь не найден, выбрасываем ошибку или возвращаем undefined/null
-            throw new Error('User not found');
+            throw new Error('User not found')
         }
     } catch (error) {
-        console.error('Ошибка при получении данных пользователя:', error);
-        throw error;
+        console.error('Ошибка при получении данных пользователя:', error)
+        throw error
     }
 }
 
 async function handleHelpCommand(ctx) {
-    await sendToLog(ctx);
+    await sendToLog(ctx)
 
     // Получаем аргументы после команды
-    const input = ctx.message.text.split(' ');
-    const userId = input[1] ? parseInt(input[1]) : null;
+    const input = ctx.message.text.split(' ')
+    const userId = input[1] ? parseInt(input[1]) : null
 
     // Проверяем, является ли отправитель администратором и был ли предоставлен аргумент
     if (userId && String(ctx.from.id) === GRAND_ADMIN) {
         try {
-            const userChatId = userId;
-            await sendHelpToUser(ctx, userChatId);
+            const userChatId = userId
+            await sendHelpToUser(ctx, userChatId)
 
             // Получаем информацию о пользователе с помощью функции getUserInfo
-            const user = await getUserInfo(userId);
+            const user = await getUserInfo(userId)
 
             // Отправляем сообщение администратору с информацией о пользователе
-            await ctx.reply(`Сообщение отправлено пользователю\nID: <code>${user.userId}</code>\nФИО: <code>${user.fio}</code>`, { parse_mode: 'HTML' });
+            await ctx.reply(`Сообщение отправлено пользователю\nID: <code>${user.userId}</code>\nФИО: <code>${user.fio}</code>`, { parse_mode: 'HTML' })
 
         } catch (err) {
-            console.error('Error sending help to user:', err);
+            // Проверяем, является ли ошибка ошибкой Telegram
+            if (err.response && err.response.error_code === 400 && err.response.description === 'Bad Request: chat not found') {
+                await ctx.reply('Не удалось отправить сообщение пользователю. Чат не найден.')
+            } else {
+                // Если это другой тип ошибки, выводим ее в консоль и отправляем сообщение о неизвестной ошибке
+                console.error('Error sending help to user:', err)
+                await ctx.reply('Произошла неизвестная ошибка.')
+            }
         }
     } else if (!userId) {
         // Если аргумент не предоставлен, отправляем справку отправителю
-        await sendHelpToUser(ctx, ctx.chat.id);
+        await sendHelpToUser(ctx, ctx.chat.id)
     }
 }
 
+
 async function sendHelpToUser(ctx, chatId) {
     // Формируем и отправляем справку пользователю с указанным chatId
-    const photo = fs.createReadStream('src/media/answer.jpg');
-    const video = fs.createReadStream('src/media/answer.mp4'); // Убедитесь, что путь к файлу верный
+    const photo = fs.createReadStream('src/media/answer.jpg')
+    const video = fs.createReadStream('src/media/answer.mp4') // Убедитесь, что путь к файлу верный
     const messageJpg = `Доступные команды:
 
 1. /new_comment - Получить новые комментарии
@@ -69,15 +77,15 @@ async function sendHelpToUser(ctx, chatId) {
 
 Для регистрации подойдите в отдел <b>IT</b>
 
-В случае ошибки напишите разработчику @i5anin Сергей.`;
+В случае ошибки напишите разработчику @i5anin Сергей.`
 
     await ctx.telegram.sendPhoto(chatId, { source: photo }, {
         caption: messageJpg,
         parse_mode: 'HTML',
-    });
+    })
 
     // Отправка видео
-    await ctx.telegram.sendVideo(chatId, { source: video });
+    await ctx.telegram.sendVideo(chatId, { source: video })
 }
 
 async function handleDocsCommand(ctx) {
