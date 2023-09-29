eb7e4a6e1e88f58da0923053d57487d09f713872
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-09-29 14:06:28 +0300

    +

--- src/modules/cron.js
+++ src/modules/cron.js
@@ -18,7 +18,7 @@ function initCronJobs(currentDateTime, instanceNumber) {
 
     if (MODE === 'build') {
         // Проверка экземпляра
-        cron.schedule('*/3 * * * *', async () => {
+        cron.schedule('*/5 * * * *', async () => {
             stateCounter.bot_check++
 
             // Получаем текущую дату и время
@@ -28,9 +28,10 @@ function initCronJobs(currentDateTime, instanceNumber) {
             try {
                 const response = await checkBotData(formattedDateTime, instanceNumber)
                 console.log('Данные о актуальном экземляре:', response.latest_entry)
+                console.log('Данные о актуальном экземляре:', response.latest_entry.date)
 
                 // Проверяем соответствие
-                if (formattedDateTime !== response.data.latest_entry.date || instanceNumber !== response.data.latest_entry.random_key) {
+                if (formattedDateTime !== response.latest_entry.date || instanceNumber !== response.latest_entry.random_key) {
                     console.error('Несоответствие данных! Останавливаем бота.')
                     await bot.telegram.sendMessage(LOG_CHANNEL_ID, emoji.x + 'Несоответствие данных! Останавливаем бота.', { parse_mode: 'HTML' })
                     // Сначала останавливаем бота
