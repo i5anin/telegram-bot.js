52368a9bf67c14190a7bbc8835d20fe84977772c
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-10-02 09:29:12 +0300

    Update notify.js

--- src/modules/notify.js
+++ src/modules/notify.js
@@ -1,13 +1,20 @@
-// Импортируем необходимые функции
 const { resetFlags } = require('#src/utils/helpers')
 const { fetchComments } = require('#src/modules/comment')
 const { sendToLog } = require('#src/utils/log')
 const { formatPaymentDate } = require('#src/utils/helpers')
-const { updateComment } = require('#src/api/index')
+const { updateComment, getAllUsers } = require('#src/api/index')
 
+let usersData = []
 
-// Функция для уведомления одного пользователя о некомментированных задачах
-// Общие вспомогательные функции
+async function loadUsersData() {
+    try {
+        const result = await getAllUsers()
+        console.log(result)
+        usersData = result.users_data || []
+    } catch (error) {
+        console.error('Failed to fetch users data:', error)
+    }
+}
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
@@ -17,14 +24,13 @@ async function sendMessage(chatId, message) {
     await sleep(500)
     try {
         await bot.telegram.sendMessage(chatId, message, { parse_mode: 'HTML' })
-        return true // Возвращаем true, если сообщение отправлено успешно
+        return true
     } catch (error) {
         console.error(`Notify. Failed to send message to chatId: ${chatId}`, error)
         if (error.code === 400 && error.description === 'Bad Request: chat not found') {
-            // Отправляем сообщение об ошибке в канал для логирования
             await bot.telegram.sendMessage(LOG_CHANNEL_ID, `Чат не найден: <code>${chatId}</code>`, { parse_mode: 'HTML' })
         }
-        return false // Возвращаем false, если произошла ошибка
+        return false
     }
 }
 
@@ -40,7 +46,6 @@ async function updateTaskStatus(id_task) {
         console.log('Notify. Error while updating task status:', error)
     }
 }
-
 
 function formatMessage(comment, total) {
     const typeMapping = {
@@ -64,40 +69,39 @@ function formatMessage(comment, total) {
         `<i>необходимо прокомментировать через "ответить" на это сообщение</i>`
 }
 
-// Первая функция
-
 async function notifyAllUsers() {
+    await loadUsersData()
     const allComments = await fetchComments()
-
-    if (!allComments) {
-        console.error('Не удалось получить комментарии')
-        return
-    }
-
     const user_ids = [...new Set(allComments.map(comment => comment.user_id))]
 
     for (const chatId of user_ids) {
-        try {
-            const userComments = allComments.filter(comment => comment.user_id === chatId && comment.sent === 0)
+        const userComments = allComments.filter(comment => comment.user_id === chatId && comment.sent === 0)
 
-            if (userComments.length === 0) continue
+        if (userComments.length === 0) continue
 
-            const message = formatMessage(userComments[0], userComments.length)
-            const isMessageSent = await sendMessage(chatId, message + '\n<code>Cron</code>')
+        const getUserName = (userId) => {
+            const user = usersData.find(u => u.user_id === userId)
+            return user ? user.fio : '<code>Неизвестный пользователь</code>'
+        }
+        const message = formatMessage(userComments[0], userComments.length)
+        const isMessageSent = await sendMessage(chatId, message + '\n<code>Cron</code>')
 
-            if (!isMessageSent) continue
+        if (isMessageSent) {
+            const masterChatId = userComments[0].user_id_master
+            if (masterChatId) {
+                const masterMessage = `Уведомление отправлено пользователю <code>${getUserName(chatId)}</code>.`
+                await sendMessage(masterChatId, masterMessage)
+            }
 
             await updateTaskStatus(userComments[0].id_task)
             await sendMessage(LOG_CHANNEL_ID, `<code>Cron</code> Отправлено пользователю <code>${chatId}</code> task_ID: <code>${userComments[0].id_task}</code>`)
-        } catch (error) {
-            console.error(`Ошибка при отправке сообщения пользователю ${chatId}:`, error)
+        } else {
+            console.error(`Ошибка при отправке сообщения пользователю ${chatId}`)
             await sendMessage(LOG_CHANNEL_ID, `Чат не найден для chatId: <code>${chatId}</code>`)
         }
         await sleep(500)
     }
 }
-
-// Вторая функция
 
 async function notifyUsers(ctx) {
     await sendToLog(ctx)
@@ -105,42 +109,46 @@ async function notifyUsers(ctx) {
     resetFlags(ctx)
     const chatId = ctx.message.chat.id
 
-    try {
-        const uncommentedTasks = await fetchComments(chatId)
+    await loadUsersData()
+    console.log('loadUsersData = ', loadUsersData())
+    const uncommentedTasks = await fetchComments(chatId)
 
-        if (!uncommentedTasks) {
-            console.error('Не удалось получить комментарии.')
-            await sendMessage(chatId, 'Произошла ошибка при получении комментариев.')
-            await sendMessage(LOG_CHANNEL_ID, `<code>${chatId}</code> Произошла ошибка при получении комментариев.`)
-            return
+    if (!uncommentedTasks) {
+        console.error('Не удалось получить комментарии.')
+        await sendMessage(chatId, 'Произошла ошибка при получении комментариев.')
+        await sendMessage(LOG_CHANNEL_ID, `<code>${chatId}</code> Произошла ошибка при получении комментариев.`)
+        return
+    }
+
+    const isUserInList = uncommentedTasks.some(task => task.user_id === chatId)
+    if (!isUserInList) {
+        await sendMessage(chatId, 'Пустые комментарии не найдены.')
+        await sendMessage(LOG_CHANNEL_ID, `<code>${chatId}</code> Пустые комментарии не найдены.`)
+        return
+    }
+
+    const userActualComments = uncommentedTasks.filter(({ user_id }) => user_id === chatId)
+    if (userActualComments.length === 0) return
+
+    const taskId = userActualComments[0].id_task
+    await sendMessage(LOG_CHANNEL_ID, `Отправлено пользователю <code>${chatId} </code> task_ID: <code>${taskId}</code>`)
+    const message = formatMessage(userActualComments[0], userActualComments.length)
+    const isMessageSent = await sendMessage(chatId, message)
+
+    const getUserName = (userId) => {
+        const user = usersData.find(u => u.user_id === userId)
+        return user ? user.fio : '<code>Неизвестный пользователь</code>'
+    }
+
+    if (isMessageSent) {
+        const masterChatId = userActualComments[0].user_id_master
+        if (masterChatId) {
+            const masterMessage = `Уведомление отправлено пользователю <code>${getUserName(chatId)}</code>.`
+            await sendMessage(masterChatId, masterMessage)
         }
-
-        const isUserInList = uncommentedTasks.some(task => task.user_id === chatId)
-
-        if (!isUserInList) {
-            ctx.session.isUserInitiated = false
-            await sendMessage(chatId, 'Пустые комментарии не найдены.')
-            await sendMessage(LOG_CHANNEL_ID, `<code>${chatId}</code> Пустые комментарии не найдены.`)
-            return
-        }
-
-        const userActualComments = uncommentedTasks.filter(({ user_id }) => user_id === chatId)
-        if (userActualComments.length === 0) return
-
-        const taskId = userActualComments[0].id_task
-        await sendMessage(LOG_CHANNEL_ID, `Отправлено пользователю <code>${chatId} </code> task_ID: <code>${taskId}</code>`)
-        const message = formatMessage(userActualComments[0], userActualComments.length)
-        const isMessageSent = await sendMessage(chatId, message)
-
-        if (isMessageSent) {
-            await updateTaskStatus(userActualComments[0].id_task)
-        }
-    } catch (error) {
-        console.log('Notify Error in notifyUsers:', error)
-        await sendMessage(LOG_CHANNEL_ID, `Notify <code>${error} </code>`)
+    } else {
+        await sendMessage(LOG_CHANNEL_ID, `Чат не найден для chatId: <code>${chatId}</code>`)
     }
 }
 
-
-// Экспортируем функции
 module.exports = { notifyUsers, notifyAllUsers }
