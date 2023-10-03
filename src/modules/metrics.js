df46c25b232a118f3d87aa9db644442ed2a0c6b0
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-10-03 11:51:26 +0300

    Update metrics.js

--- src/modules/metrics.js
+++ src/modules/metrics.js
@@ -1,54 +1,75 @@
-// const axios = require('axios');
+// Подключение необходимых библиотек и модулей
+// const axios = require('axios'); // Если вы используете axios
+const fetch = require('node-fetch');  // Если вы используете fetch
 
+// Функция для получения метрик
 async function fetchMetrics() {
     try {
         const response = await fetch(`${WEB_API}/metrics/get.php?key=${SECRET_KEY}`);
-        const data = await response.json();  // Добавьте эту строку
-        return data.metrics;  // Измените response.data.metrics на data.metrics
+        const data = await response.json();
+        return data.metrics;
     } catch (error) {
         throw new Error(`Failed to fetch metrics: ${error.message}`);
     }
 }
 
-function formatNumber(number) {
+// Функция для форматирования числа
+function formatNumber(number, isFractional) {
     return parseFloat(number).toLocaleString('ru-RU', {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    })
+        minimumFractionDigits: isFractional ? 2 : 0,
+        maximumFractionDigits: isFractional ? 2 : 0,
+    });
 }
 
+// Функция для отправки уведомления с метриками
 async function sendMetricsNotification() {
     try {
-        const metrics = await fetchMetrics()
+        const metrics = await fetchMetrics();
 
-        let message = ''
-        metrics.forEach(metric => {
-            const formattedValue = formatNumber(metric.value)
-            message += `${metric.name}: <b>${formattedValue}</b> ${metric.unit}\n`
-        })
+        let message = '';
+        let groupTitle = '';
+        metrics.forEach((metric, index) => {
+            const isFractional = metric.unit.includes('/');
+            const formattedValue = formatNumber(metric.value, isFractional);
 
-        const ADMIN_IDS = [OPLATA_GROUP]
+            if (metric.name === 'Не завершённое по М/О') {
+                groupTitle = '\n' + metric.name + ': \n';
+            } else if (metric.name === 'Итого внутреннего производства') {
+                groupTitle = '\n' + metric.name + ': ';
+            } else if (metric.name === 'Отклонение от плана Производства') {
+                groupTitle = '\nОтклонение от плана \n';
+            } else if (metric.name === 'Воронка Производство') {
+                groupTitle = '\nВоронка\n';
+            } else {
+                groupTitle = '';
+            }
+
+            message += `${groupTitle}<b>${metric.name}:</b> ${formattedValue} ${metric.unit}\n`;
+        });
+
+        const ADMIN_IDS = [OPLATA_GROUP];
         for (const adminId of ADMIN_IDS) {
             try {
-                await bot.telegram.sendMessage(adminId, message, { parse_mode: 'HTML' })
-                console.log('Message sent successfully to adminId:', adminId)
+                await bot.telegram.sendMessage(adminId, message, { parse_mode: 'HTML' });
+                console.log('Message sent successfully to adminId:', adminId);
             } catch (error) {
-                console.error('Failed to send message to adminId:', adminId, 'Error:', error)
+                console.error('Failed to send message to adminId:', adminId, 'Error:', error);
                 await bot.telegram.sendMessage(
                     LOG_CHANNEL_ID,
                     `Failed to send metrics message to <code>${adminId}</code>\n<code>${error}</code>`,
-                    { parse_mode: 'HTML' },
-                )
+                    { parse_mode: 'HTML' }
+                );
             }
         }
     } catch (error) {
-        console.error('Error fetching or sending metrics:', error)
+        console.error('Error fetching or sending metrics:', error);
         await bot.telegram.sendMessage(
             LOG_CHANNEL_ID,
             `Error fetching or sending metrics\n<code>${error}</code>`,
-            { parse_mode: 'HTML' },
-        )
+            { parse_mode: 'HTML' }
+        );
     }
 }
 
-module.exports = { sendMetricsNotification }
+// Экспорт функции для использования в других модулях
+module.exports = { sendMetricsNotification };
