6fdfe369f9228863bbed499bd9233cac93f9be46
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-10-05 12:16:43 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -32,9 +32,24 @@ $mysqli->close();
   <link rel="icon" type="image/png" href="assets/favicon.svg">
   <link href="css/bootstrap.css" rel="stylesheet">
   <style>
-    .centered-image {
-      display: flex;
-      justify-content: center;
+    .text-cell {
+      max-width: 150px;
+      /* Максимальная ширина ячейки */
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    .table-responsive {
+      height: 100vh;
+      overflow-y: auto;
+    }
+
+    thead th {
+      position: sticky;
+      top: 0;
+      background-color: #f8f9fa;
+      cursor: pointer;
     }
   </style>
 </head>
@@ -68,20 +83,20 @@ $mysqli->close();
           while ($row = $result->fetch_assoc()) : ?>
             <tr>
               <th scope="row"><?php echo $rowNumber; ?></th>
-              <td><?php echo $row['id_task']; ?></td>
-              <td><?php echo $row['comments_op']; ?></td>
-              <td><?php echo $row['completed']; ?></td>
-              <td><?php echo $row['date']; ?></td>
-              <td><?php echo $row['smena_id']; ?></td>
-              <td><?php echo $row['specs_nom_id']; ?></td>
-              <td><?php echo $row['det_name']; ?></td>
-              <td><?php echo $row['user_id']; ?></td>
-              <td><?php echo $row['type']; ?></td>
-              <td><?php echo $row['kolvo_brak']; ?></td>
-              <td><?php echo $row['comments_otk']; ?></td>
-              <td><?php echo $row['sent']; ?></td>
-              <td><?php echo $row['answered']; ?></td>
-              <td><?php echo $row['user_id_master']; ?></td>
+              <td class="text-cell"><?php echo $row['id_task']; ?></td>
+              <td class="text-cell"><?php echo $row['comments_op']; ?></td>
+              <td class="text-cell"><?php echo $row['completed']; ?></td>
+              <td class="text-cell"><?php echo $row['date']; ?></td>
+              <td class="text-cell"><?php echo $row['smena_id']; ?></td>
+              <td class="text-cell"><?php echo $row['specs_nom_id']; ?></td>
+              <td class="text-cell"><?php echo $row['det_name']; ?></td>
+              <td class="text-cell"><?php echo $row['user_id']; ?></td>
+              <td class="text-cell"><?php echo $row['type']; ?></td>
+              <td class="text-cell"><?php echo $row['kolvo_brak']; ?></td>
+              <td class="text-cell"><?php echo $row['comments_otk']; ?></td>
+              <td class="text-cell"><?php echo $row['sent']; ?></td>
+              <td class="text-cell"><?php echo $row['answered']; ?></td>
+              <td class="text-cell"><?php echo $row['user_id_master']; ?></td>
             </tr>
           <?php
             $rowNumber++;  // Increment the row number after each row
@@ -90,6 +105,28 @@ $mysqli->close();
       </table>
     </div>
   </div>
+
+  <script>
+    document.querySelectorAll('thead th').forEach(headerCell => {
+      headerCell.addEventListener('click', () => {
+        const tableElement = headerCell.closest('table');
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains('ascending');
+
+        Array.from(tableElement.querySelectorAll('tbody tr'))
+          .sort((trA, trB) => {
+            const cellA = trA.children[headerIndex].textContent;
+            const cellB = trB.children[headerIndex].textContent;
+            return currentIsAscending ?
+              cellA.localeCompare(cellB) :
+              cellB.localeCompare(cellA);
+          })
+          .forEach(tr => tableElement.querySelector('tbody').appendChild(tr));
+
+        headerCell.classList.toggle('ascending', !currentIsAscending);
+      });
+    });
+  </script>
 </body>
 
 </html>
