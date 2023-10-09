45824cb6616baad6a7197d3635836906be62ab5d
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-10-09 16:56:05 +0300

    фото обработка

--- src/modules/photo.js
+++ src/modules/photo.js
@@ -7,46 +7,55 @@ const BOT_TOKEN = process.env.BOT_TOKEN
 async function handlePhoto(ctx) {
     // Проверяем, есть ли фотографии в сообщении
     if (ctx.message.photo && ctx.message.photo.length > 0) {
-        // Выбираем последнюю фотографию в массиве, которая является самой большой
-        const photo = ctx.message.photo[ctx.message.photo.length - 1];
-        const photoFileId = photo.file_id;
-        const photoInfo = await ctx.telegram.getFile(photoFileId);
-        const caption = ctx.message.caption;
-
-        // Проверяем наличие подписи
-        if (!caption) {
-            ctx.reply('Извините, подпись к фотографии обязательна.\nПопробуйте снова с подписью.');
-            return;
-        }
-
-        const currentDate = new Date()
-            .toISOString()
-            .replace(/:/g, '_')
-            .replace(/\.\d+Z$/, '');
-        const fileName = `${currentDate}_${ctx.from.id}.jpg`;
-
+        const caption = ctx.message.caption
         // Проверяем, зарегистрирован ли пользователь
-        const userData = await checkUser(ctx.from.id);
-
+        const userData = await checkUser(ctx.from.id)
         if (userData.exists) {
             // Пользователь зарегистрирован, можно продолжить сохранение фотографии
-            const filePath = path.join('D:', 'db_photo', fileName);
-            const fileStream = fs.createWriteStream(filePath);
-            const url =
-                `https://api.telegram.org/file/bot${BOT_TOKEN}/${photoInfo.file_path}`;
-            const request = require('request');
+            for (let i = 0; i < ctx.message.photo.length; i++) {
+                const photo = ctx.message.photo[i]
+                // Проверяем, существует ли фотография
+                if (photo && photo.length > 0) {
+                    // Выбираем самую большую версию фотографии
+                    const photoFileId = photo[photo.length - 1].file_id
+                    const photoInfo = await ctx.telegram.getFile(photoFileId)
 
-            // Загружаем фотографию в локальную директорию
-            request(url).pipe(fileStream);
+                    const currentDate = new Date().toISOString().replace(/:/g, '_').replace(/\.\d+Z$/, '')
+                    const fileName = `${currentDate}_${ctx.from.id}_${i}.jpg`
 
-            ctx.reply('Фотография успешно сохранена.');
+                    const filePath = path.join('D:', 'db_photo', fileName)
+                    const fileStream = fs.createWriteStream(filePath)
+                    const url = `https://api.telegram.org/file/bot${BOT_TOKEN}/${photoInfo.file_path}`
+
+                    // Загружаем фотографию в локальную директорию
+                    request(url).pipe(fileStream)
+                    ctx.session.photoParty = true
+                    // Send a message only once
+                    // ctx.reply(`Фотографии успешно сохранены. Всего обработано: ${ctx.message.photo.length}`);
+                    ctx.reply('Пожалуйста, введите номер партии.');
+                    return;  // Return to prevent further processing
+                }
+            }
+
+            // Отправляем сообщение только после того, как все фотографии были обработаны
+            // ctx.reply(`Фотографии успешно сохранены. Всего обработано: ${ctx.message.photo.length}`)
+            ctx.reply('Пожалуйста, введите номер партии.')
+            ctx.session.photoParty = true
         } else {
             // Пользователь не зарегистрирован, обработка ошибки
-            ctx.reply('Извините, вы не зарегистрированы. Обратитесь к администратору.');
+            ctx.reply('Извините, вы не зарегистрированы. Обратитесь к администратору.')
         }
     } else {
-        ctx.reply('Извините, я ожидал фотографию. Попробуйте снова.');
+        ctx.reply('Извините, я ожидал фотографию. Попробуйте снова.')
     }
 }
 
-module.exports = {handlePhoto}
+module.exports = { handlePhoto }
+
+
+
+
+
+
+
+
--- src/modules/text.js
+++ src/modules/text.js
@@ -5,63 +5,73 @@ const { notifyUsers } = require('#src/modules/notify')
 const { sendToLog } = require('#src/utils/log')
 const { addUser } = require('#src/api/index')
 
-async function handleTextCommand(ctx) {
-    await sendToLog(ctx)
-    if (ctx.chat.type !== 'private') return
-    // Деструктуризация полей из сообщения
-    const { text, chat, from } = ctx.message
+async function handleFio(ctx, text, chat, from) {
+    console.log('ctx.session.isAwaitFio=', ctx.session.isAwaitFio)
+    if (!/^[А-Яа-яёЁëË]+\s[А-Яа-яёЁëË]\. ?[А-Яа-яёЁëË]\.$/.test(text)) {
+        ctx.reply(ruLang.invalidData)
+        return
+    }
+    const cleanedText = text.replace(/ë/g, 'ё').replace(/Ë/g, 'Ё').replace(/\. /g, '.')
+    const userId = chat.id
 
-    // Ранний выход для улучшения читаемости
-    if (!ctx.session.isAwaitFio && !ctx.session.isAwaitComment && !ctx.message.reply_to_message) return
-
-    // --------- Обработка ожидания ФИО ---------
-    if (ctx.session.isAwaitFio && ctx.chat.type === 'private') {
-        console.log('ctx.session.isAwaitFio=', ctx.session.isAwaitFio)
-        if (!/^[А-Яа-яёЁëË]+\s[А-Яа-яёЁëË]\. ?[А-Яа-яёЁëË]\.$/.test(text)) { //налог с диакритическим знаком "ë"
-            ctx.reply(ruLang.invalidData)
-            return
-        }
-        // Дальнейшая логика обработки ФИО
-        const cleanedText = text.replace(/ë/g, 'ё').replace(/Ë/g, 'Ё').replace(/\. /g, '.')
-        const userId = chat.id
-
-        try {
-            // Запрос на добавление пользователя
-            const dataAddUser = await addUser(userId, cleanedText, from.username)
-            console.log('dataAddUser=', dataAddUser)
-            if (dataAddUser && dataAddUser.status === 'OK') {
-                ctx.reply('Вы успешно зарегистрированы', { parse_mode: 'HTML' })
-                const defMsg = `\nID: <code>${userId}</code>` +
-                    `\nfio: <code>${cleanedText}</code>`
-
-                await bot.telegram.sendMessage(
-                    LOG_CHANNEL_ID, `${emoji.star}Пользователь добавлен.${defMsg}`,
-                    { parse_mode: 'HTML' },
-                )
-                await notifyUsers(ctx)
-            } else {
-                throw new Error(dataAddUser ? dataAddUser.message : '\nНеизвестная ошибка при добавлении пользователя.')
-            }
-        } catch (error) {
-            console.error('Ошибка при добавлении пользователя:', error.message);
-            ctx.reply(`Ошибка регистрации: ${error.message}`, { parse_mode: 'HTML' });
+    try {
+        const dataAddUser = await addUser(userId, cleanedText, from.username)
+        console.log('dataAddUser=', dataAddUser)
+        if (dataAddUser && dataAddUser.status === 'OK') {
+            ctx.reply('Вы успешно зарегистрированы', { parse_mode: 'HTML' })
             const defMsg = `\nID: <code>${userId}</code>` +
                 `\nfio: <code>${cleanedText}</code>`
 
             await bot.telegram.sendMessage(
-                LOG_CHANNEL_ID, `⚠️Ошибка регистрации${defMsg}`,
+                LOG_CHANNEL_ID, `${emoji.star}Пользователь добавлен.${defMsg}`,
                 { parse_mode: 'HTML' },
             )
+            await notifyUsers(ctx)
+        } else {
+            throw new Error(dataAddUser ? dataAddUser.message : '\nНеизвестная ошибка при добавлении пользователя.')
         }
-        ctx.session.isAwaitFio = false
+    } catch (error) {
+        console.error('Ошибка при добавлении пользователя:', error.message)
+        ctx.reply(`Ошибка регистрации: ${error.message}`, { parse_mode: 'HTML' })
+        const defMsg = `\nID: <code>${userId}</code>` +
+            `\nfio: <code>${cleanedText}</code>`
+
+        await bot.telegram.sendMessage(
+            LOG_CHANNEL_ID, `⚠️Ошибка регистрации${defMsg}`,
+            { parse_mode: 'HTML' },
+        )
     }
+    ctx.session.isAwaitFio = false
+
+}
+
+async function photoParty(ctx, text) {
+    console.log('photoParty function called with text:', text)
+    ctx.session.batchNumber = text
+    ctx.reply(`Вы ввели номер партии: <code>${ctx.session.batchNumber}</code>. Пожалуйста, введите комментарий.`, { parse_mode: 'HTML' })
+    ctx.session.photoParty = false
+    ctx.session.photoMessage = true
+}
+
+async function photoMessageComment(ctx) {
+    console.log('photoMessageComment function called')
+    const { text } = ctx.message
+    ctx.session.photoComment = text
+    ctx.reply(`Спасибо! Ваш комментарий к фотографии: <code>${ctx.session.photoComment}</code> номер партии: <code>${ctx.session.batchNumber}</code>`, { parse_mode: 'HTML' })
+    // TODO: ОТПРАВКА ДАННЫХ НА СЕРВЕР
+    ctx.session.step = ''
+    ctx.session.photoMessage = false
+}
 
 
-    // --------- Обработка ожидания комментария ---------
-    if (ctx.message.reply_to_message) {
-        await handleAddComment(ctx)
-        console.log('Обработка ожидания комментария handleAddComment')
-    }
+async function handleTextCommand(ctx) {
+    await sendToLog(ctx)
+    if (ctx.chat.type !== 'private') return
+    const { text, chat, from } = ctx.message
+    if (ctx.session.isAwaitFio) return await handleFio(ctx, text, chat, from)
+    if (ctx.message.reply_to_message) return await handleAddComment(ctx)
+    if (ctx.session.photoParty) return await photoParty(ctx, text)
+    if (ctx.session.photoMessage) return await photoMessageComment(ctx)
 }
 
 module.exports = { handleTextCommand }
