655b1585697b24a7a690ffd87414a12a3b381391
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-10 13:35:49 +0300

    Update ru_lang.js

--- src/utils/ru_lang.js
+++ src/utils/ru_lang.js
@@ -24,17 +24,17 @@ module.exports = {
         `· <b>Дата:</b> <code>${formattedDate}</code>\n\n`,
     formatMetricsMessage: (latestMetrics, maxCharacters) =>
         `Дата: <b>${moment(latestMetrics.date, 'YYYY-MM-DD HH:mm:ss').format('DD.MM.YYYY HH:mm:ss')}</b>\n\n` +
-        `Незавершённое по М/О: <b>${formatNumber(latestMetrics.prod_price_mzp)}</b> ₽\n` +
-        `Слесарный участок: <b>${formatNumber(latestMetrics.prod_price_sles)}</b> ₽\n` +
-        `ОТК: <b>${formatNumber(latestMetrics.prod_price_otk)}</b> ₽\n` +
-        `Упаковка: <b>${formatNumber(latestMetrics.prod_price_upk)}</b> ₽\n` +
-        `Доработка ЧПУ: <b>${formatNumber(latestMetrics.prod_price_dorabotka)}</b> ₽\n` +
-        `Доработка в слесарном: <b>${formatNumber(latestMetrics.prod_price_dorabotka_sles)}</b> ₽\n` +
-        `Согласование: <b>${formatNumber(latestMetrics.prod_price_sogl)}</b> ₽\n\n` +
-        `Итого внутреннего производства: <b>${formatNumber(latestMetrics.prod_price)}</b> ₽\n` +
-        `Ожидаемая предоплата с НДС: <b>${formatNumber(latestMetrics.predoplata)}</b> ₽\n` +
-        `Итого внутреннего производства с НДС: <b>${formatNumber(latestMetrics.total_price)}</b> ₽\n` +
-        `Готовая продукция на складе с НДС: <b>${formatNumber(latestMetrics.total_sklad_gp)}</b> ₽\n\n` +
+        `Незавершённое по М/О: <b>${formatNumber(latestMetrics.prod_price_mzp)}</b>\u00A0₽\n` +
+        `Слесарный участок: <b>${formatNumber(latestMetrics.prod_price_sles)}</b>\u00A0₽\n` +
+        `ОТК: <b>${formatNumber(latestMetrics.prod_price_otk)}</b>\u00A0₽\n` +
+        `Упаковка: <b>${formatNumber(latestMetrics.prod_price_upk)}</b>\u00A0₽\n` +
+        `Доработка ЧПУ: <b>${formatNumber(latestMetrics.prod_price_dorabotka)}</b>\u00A0₽\n` +
+        `Доработка в слесарном: <b>${formatNumber(latestMetrics.prod_price_dorabotka_sles)}</b>\u00A0₽\n` +
+        `Согласование: <b>${formatNumber(latestMetrics.prod_price_sogl)}</b>\u00A0₽\n\n` +
+        `Итого внутреннего производства: <b>${formatNumber(latestMetrics.prod_price)}</b>\u00A0₽\n` +
+        `Ожидаемая предоплата с НДС: <b>${formatNumber(latestMetrics.predoplata)}</b>\u00A0₽\n` +
+        `Итого внутреннего производства с НДС: <b>${formatNumber(latestMetrics.total_price)}</b>\u00A0₽\n` +
+        `Готовая продукция на складе с НДС: <b>${formatNumber(latestMetrics.total_sklad_gp)}</b>\u00A0₽\n\n` +
         `<u>Отклонение от плана</u>\n` +
         `<code>${formatPercentage(latestMetrics.cumulative_sklad_month, maxCharacters)}</code> Производство\n` +
         `<code>${formatPercentage(latestMetrics.cumulative_brak_month, maxCharacters)}</code> Брак\n` +
@@ -44,9 +44,9 @@ module.exports = {
         `<code>${formatPercentage(latestMetrics.sles, maxCharacters)}</code> Слесарный участок\n` +
         `<code>${formatPercentage(latestMetrics.otk, maxCharacters)}</code> ОТК\n` +
         `<code>${formatPercentage(latestMetrics.upk, maxCharacters)}</code> Упаковка\n\n` +
-        `Продуктивность: <b>${formatNumber(latestMetrics.productivity)}</b> ₽/час\n` +
-        `Отгрузка М/О: <b>${formatNumber(latestMetrics.get_sum_otgr_prod)}</b> ₽\n` +
-        `Отгрузка с НДС: <b>${formatNumber(latestMetrics.get_sum_otgr)}</b> ₽\n`,
+        `Продуктивность: <b>${formatNumber(latestMetrics.productivity)}</b>\u00A0₽/час\n` +
+        `Отгрузка М/О: <b>${formatNumber(latestMetrics.get_sum_otgr_prod)}</b>\u00A0₽\n` +
+        `Отгрузка с НДС: <b>${formatNumber(latestMetrics.get_sum_otgr)}</b>\u00A0₽\n`,
 
     formatMetricsMessageFrez: (latestMetrics, maxCharacters) =>
         `Дата: <b>${moment(latestMetrics.date, 'YYYY-MM-DD HH:mm:ss').format('DD.MM.YYYY HH:mm:ss')}</b>\n\n` +
