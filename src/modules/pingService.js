0079fbc297b3c3289ffef6c37e1e7acc7cc91df0
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-03-27 14:04:08 +0300

    Update pingService.js

--- src/modules/pingService.js
+++ src/modules/pingService.js
@@ -1,45 +1,50 @@
 // pingService.js
 
-const ping = require('ping');
+const ping = require('ping')
+
 
 const hosts = [
-    { name: "Маршрутизатор 1", ip: '192.168.0.2' },
-    { name: "Маршрутизатор 2", ip: '192.168.1.9' },
-    { name: "Сервер 1", ip: '192.168.0.200' },
-    { name: "Сервер 2", ip: '192.168.0.10' },
-    { name: "Принтер", ip: '192.168.0.97' },
-    { name: "Устройство хранения данных", ip: '192.168.0.3' }
-];
+    { name: 'Маршрутизатор 1', ip: '192.168.0.2' },
+    { name: 'Маршрутизатор 2', ip: '192.168.1.9' },
+    { name: 'Сервер 1', ip: '192.168.0.200' },
+    { name: 'Устройство хранения данных', ip: '192.168.0.10' },
+    { name: 'Устройство хранения данных', ip: '192.168.0.97' },
+    { name: 'Устройство хранения данных', ip: '192.168.0.3' },
+]
 
 const pingHosts = async (hosts, numOfPings = 4) => {
-    let pingResults = '';
+    let pingResults = ''
 
     for (let host of hosts) {
-        let pings = [];
+        let pings = []
         for (let i = 0; i < numOfPings; i++) {
-            const res = await ping.promise.probe(host.ip, { timeout: 2 });
-            if (res.alive) pings.push(res.time);
+            const res = await ping.promise.probe(host.ip, { timeout: 2 })
+            if (res.alive) pings.push(res.time)
         }
 
         if (pings.length > 0) {
-            const min = Math.min(...pings);
-            const max = Math.max(...pings);
-            const avg = pings.reduce((a, b) => a + b, 0) / pings.length;
-            pingResults += `${host.name} (${host.ip})<blockquote>min: ${min}ms, avg: ${avg.toFixed(2)}ms, max: ${max}ms</blockquote>\n`;
+            const min = Math.min(...pings)
+            const max = Math.max(...pings)
+            const avg = pings.reduce((a, b) => a + b, 0) / pings.length
+            pingResults += `${host.name} (${host.ip})<blockquote>min: ${min}ms, avg: ${avg.toFixed(2)}ms, max: ${max}ms</blockquote>\n`
         } else {
-            pingResults += `${host.name} (${host.ip}), все запросы пинга тайм-аут.\n\n`;
+            pingResults += `${host.name} (${host.ip}), все запросы пинга тайм-аут.\n\n`
         }
     }
 
-    return pingResults;
-};
+    return pingResults
+}
 
-// Функция, обрабатывающая команду `/ping_test`
+// Функция, обрабатывающая команду `/ping_test` с проверкой на гранд админа
 const pingService = async (ctx) => {
-    ctx.reply('Начинаю тестирование сети, это может занять некоторое время...');
-    const results = await pingHosts(hosts);
-    // Используйте replyWithHTML для отправки HTML-форматированного текста
-    ctx.replyWithHTML(`Результаты пинга:\n ${results}`);
-};
+    // Сравнение ID отправителя команды с ID гранд админа
+    if (ctx.from.id.toString() === GRAND_ADMIN) {
+        ctx.reply('Начинаю тестирование сети, это может занять некоторое время...')
+        const results = await pingHosts(hosts)
+        ctx.replyWithHTML(`Результаты пинга:\n${results}`)
+    } else {
+        ctx.reply('Извините, у вас нет прав для выполнения этой команды.')
+    }
+}
 
-module.exports = { pingService };
+module.exports = { pingService }
