51eed7b36eab27dc02ee901457bf033d9c9a39c7
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-03-27 13:54:12 +0300

    Update pingService.js

--- src/modules/pingService.js
+++ src/modules/pingService.js
@@ -2,15 +2,22 @@
 
 const ping = require('ping');
 
-const ipList = ['192.168.0.2', '192.168.1.9', '192.168.0.200', '192.168.0.10', '192.168.0.97', '192.168.0.3'];
+const hosts = [
+    { name: "Маршрутизатор 1", ip: '192.168.0.2' },
+    { name: "Маршрутизатор 2", ip: '192.168.1.9' },
+    { name: "Сервер 1", ip: '192.168.0.200' },
+    { name: "Сервер 2", ip: '192.168.0.10' },
+    { name: "Принтер", ip: '192.168.0.97' },
+    { name: "Устройство хранения данных", ip: '192.168.0.3' }
+];
 
-const pingHosts = async (ipList, numOfPings = 4) => {
+const pingHosts = async (hosts, numOfPings = 4) => {
     let pingResults = '';
 
-    for (let ip of ipList) {
+    for (let host of hosts) {
         let pings = [];
         for (let i = 0; i < numOfPings; i++) {
-            const res = await ping.promise.probe(ip, { timeout: 2 });
+            const res = await ping.promise.probe(host.ip, { timeout: 2 });
             if (res.alive) pings.push(res.time);
         }
 
@@ -18,9 +25,9 @@ const pingHosts = async (ipList, numOfPings = 4) => {
             const min = Math.min(...pings);
             const max = Math.max(...pings);
             const avg = pings.reduce((a, b) => a + b, 0) / pings.length;
-            pingResults += `IP: ${ip}, min: ${min}ms, avg: ${avg.toFixed(2)}ms, max: ${max}ms\n`;
+            pingResults += `${host.name} (${host.ip})<blockquote>min: ${min}ms, avg: ${avg.toFixed(2)}ms, max: ${max}ms</blockquote>\n`;
         } else {
-            pingResults += `IP: ${ip}, все запросы пинга тайм-аут.\n`;
+            pingResults += `${host.name} (${host.ip}), все запросы пинга тайм-аут.\n\n`;
         }
     }
 
@@ -30,8 +37,9 @@ const pingHosts = async (ipList, numOfPings = 4) => {
 // Функция, обрабатывающая команду `/ping_test`
 const pingService = async (ctx) => {
     ctx.reply('Начинаю тестирование сети, это может занять некоторое время...');
-    const results = await pingHosts(ipList);
-    ctx.reply(`Результаты пинга:\n${results}`);
+    const results = await pingHosts(hosts);
+    // Используйте replyWithHTML для отправки HTML-форматированного текста
+    ctx.replyWithHTML(`Результаты пинга:\n ${results}`);
 };
 
 module.exports = { pingService };
