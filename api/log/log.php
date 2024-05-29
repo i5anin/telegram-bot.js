f31c93528ee9aac682f2c9515b5a122fb4d3a3a0
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-05-29 15:06:15 +0300

    Revert "Update log.php"
    This reverts commit b3248be523120eaf729d9393f73801949ee4629e.

--- api/log/log.php
+++ api/log/log.php
@@ -2,7 +2,6 @@
 
 require_once 'sql_config.php';
 
-// Функция для отправки JSON ответа
 function sendJsonResponse($status, $message, $exit = true) {
     http_response_code($status);
     echo json_encode(['error' => $status !== 200, 'message' => $message]);
@@ -11,15 +10,9 @@ function sendJsonResponse($status, $message, $exit = true) {
     }
 }
 
-// Логирование сообщения в файл
-function logMessage($message) {
-    file_put_contents('log.txt', date('Y-m-d H:i:s') . ' - ' . $message . "\n", FILE_APPEND);
-}
-
 $mysqli = new mysqli($dbConfig['server'], $dbConfig['user'], $dbConfig['pass'], $dbConfig['db']);
 
 if ($mysqli->connect_error) {
-    logMessage("Ошибка подключения к базе данных: " . $mysqli->connect_error);
     sendJsonResponse(500, "Ошибка подключения к базе данных: " . $mysqli->connect_error);
 }
 
@@ -35,26 +28,19 @@ $type = $data['type'] ?? null;
 $info = $data['info'] ?? null;
 
 if (!$userID || !$text || $error === null || $ok === null || !$type || !$info) {
-    logMessage('Отсутствуют обязательные поля');
     sendJsonResponse(400, 'Отсутствуют обязательные поля');
 }
 
 $query = "INSERT INTO `log` (`user_id`, `date_time_event`, `text`, `error`, `ok`, `type`, `info`) VALUES (?, NOW(), ?, ?, ?, ?, ?)";
 
 if ($stmt = $mysqli->prepare($query)) {
-    if (!$stmt->bind_param('isisss', $userID, $text, $error, $ok, $type, $info)) {
-        logMessage("Ошибка bind_param: " . $stmt->error);
-    }
+    $stmt->bind_param('isisss', $userID, $text, $error, $ok, $type, $info);
     if (!$stmt->execute()) {
-        logMessage("Ошибка добавления записи лога: " . $stmt->error);
         sendJsonResponse(500, "Ошибка добавления записи лога: " . $stmt->error);
-    } else {
-        logMessage("Запись лога добавлена успешно");
-        sendJsonResponse(200, "Запись лога добавлена успешно");
     }
     $stmt->close();
+    sendJsonResponse(200, "Запись лога добавлена успешно");
 } else {
-    logMessage("Ошибка подготовки SQL запроса: " . $mysqli->error);
     sendJsonResponse(500, "Ошибка подготовки SQL запроса: " . $mysqli->error);
 }
 
