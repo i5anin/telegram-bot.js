414adc78bef8dc00e4afae95feec0dfa3741a23b
Isanin Sergey <sergey.isanin@gmail.com>
2024-06-03 23:32:03 +0300

    Update web.php

--- api/metrics/web.php
+++ api/metrics/web.php
@@ -7,6 +7,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
     <style>
 
         body {
