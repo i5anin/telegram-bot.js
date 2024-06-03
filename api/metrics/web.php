9a1c9b3254e6ec0cb5c81c1642d7f9b06ee732ca
Isanin Sergey <sergey.isanin@gmail.com>
2024-06-03 19:21:45 +0300

    Update web.php

--- api/metrics/web.php
+++ api/metrics/web.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Telegram App Metrics</title>
+    <title>Telegram App Metrics</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <style>
@@ -17,6 +18,18 @@
 
         .data-table {
             margin-top: 20px;
+        }
+
+        .table .warning {
+            color: red;
+        }
+
+        .table .plan-deviation {
+            color: blue;
+        }
+
+        .table .funnel {
+            color: green;
         }
 
         @media (max-width: 768px) {
@@ -33,7 +46,7 @@
 </head>
 <body>
 <div class="container">
-<!--    <h1 class="text-center">Telegram App Metrics</h1>-->
+    <!--    <h1 class="text-center">Telegram App Metrics</h1>-->
     <div class="d-flex justify-content-center">
         <div class="btn-group" role="group">
             <input type="radio" class="btn-check" name="date_filter" id="today" value="today" checked>
@@ -46,6 +59,8 @@
         <table class="table table-striped table-bordered">
             <thead>
             <tr>
+                <th>Название</th>
+                <th>Значение</th>
             </tr>
             </thead>
             <tbody id="metrics-data"></tbody>
@@ -80,6 +95,23 @@
         'get_sum_otgr': 'Отгрузка',
     };
 
+    function formatNumber(number) {
+        return number.toLocaleString('ru-RU', { minimumFractionDigits: 2 });
+    }
+
+    function formatPercentage(value, maxCharacters) {
+        const formattedValue = `${(value * 100).toFixed(2)}%`;
+        return formattedValue.substring(0, maxCharacters);
+    }
+
+    function checkWarningAndFormat(value, label) {
+        if (value <= 3400000) {
+            return `<span class="warning">${formatNumber(value)}</span>`;
+        } else {
+            return formatNumber(value);
+        }
+    }
+
     async function fetchMetrics(dateFilter) {
         try {
             const response = await fetch(`get.php?key=SecretKeyPFForum23&dateFilter=${dateFilter}`)
@@ -109,7 +141,30 @@
                 const nameCell = row.insertCell()
                 const valueCell = row.insertCell()
                 nameCell.textContent = humanNames[key] || key
-                valueCell.textContent = metric[key]
+
+                if (key === 'date') {
+                    valueCell.textContent = moment(metric[key]).format('DD.MM.YYYY HH:mm:ss');
+                } else if (['prod_price_mzp', 'prod_price', 'predoplata', 'total_price', 'total_sklad_gp', 'get_sum_otgr_prod', 'get_sum_otgr', 'productivity_prod', 'productivity'].includes(key)) {
+                    valueCell.textContent = `${formatNumber(metric[key])} ₽`;
+                    valueCell.classList.add('currency')
+                } else if (['cumulative_sklad_month', 'cumulative_brak_month', 'cumulative_manager_month', 'prod', 'sles', 'otk', 'upk'].includes(key)) {
+                    valueCell.textContent = `${formatPercentage(metric[key], 10)}`;
+                    valueCell.classList.add('percent')
+                } else {
+                    valueCell.textContent = metric[key]
+                }
+
+                if (['prod_price_sles', 'prod_price_otk', 'prod_price_upk', 'prod_price_dorabotka', 'prod_price_dorabotka_sles', 'prod_price_sogl'].includes(key)) {
+                    valueCell.classList.add('warning')
+                }
+
+                if (['cumulative_sklad_month', 'cumulative_brak_month', 'cumulative_manager_month'].includes(key)) {
+                    valueCell.classList.add('plan-deviation')
+                }
+
+                if (['prod', 'sles', 'otk', 'upk'].includes(key)) {
+                    valueCell.classList.add('funnel')
+                }
             }
         }
     }
