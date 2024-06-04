68a524534645cca9c10c402f779adca184853843
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-06-04 08:40:21 +0300

    Update log.php

--- api/log/log.php
+++ api/log/log.php
@@ -21,7 +21,7 @@ $currentDateTime = date('Y-m-d H:i:s');
 // Проверяем соединение
 if ($mysqli->connect_error) {
     http_response_code(500);
-    echo json_encode(['error' => "Connection failed: " . $mysqli->connect_error]); // Сообщение об ошибке
+    echo json_encode(['error' => "Connection failed: " . $mysqli->connect_error]);
     exit;
 }
 
@@ -30,25 +30,18 @@ $mysqli->set_charset('utf8mb4');
 // Получаем и декодируем JSON тело POST запроса
 $data = json_decode(file_get_contents('php://input'), true);
 
-// Извлекаем необходимые данные из $data
+// Извлекаем данные из $data, присваиваем null, если данных нет
 $userID = $data['user_id'] ?? null;
 $groupID = $data['group_id'] ?? null;
 $text = $data['text'] ?? null;
 $error = $data['error'] ?? null;
 $ok = $data['ok'] ?? null;
-$type = $data['type'] ?? null;
-$fio = $data['fio'] ?? null; // Изменили 'info' на 'fio'
-$groupName = $data['group_name'] ?? null; // Добавили поле 'group_name'
-$test = $data['test'] ?? 1;
+$type = $data['type'] ?? null; // Убираем значение по умолчанию
+$fio = $data['fio'] ?? null; // Тоже убираем значение по умолчанию
+$groupName = $data['group_name'] ?? null;
+$test = $data['test'] ?? null;
 
-// Проверяем, были ли предоставлены необходимые данные
-if (!$userID || !$groupID || !$text || !isset($error) || !isset($ok) || !isset($type) || !isset($fio) || $groupName === null) { // Проверка всех полей
-    http_response_code(400);
-    echo json_encode(['error' => 'Missing required fields`']);
-    exit;
-}
-
-// Выводим полученные данные
+// Выводим полученные данные для проверки
 echo json_encode([
     'user_id' => $userID,
     'group_id' => $groupID,
@@ -61,14 +54,11 @@ echo json_encode([
     'test' => $test
 ]);
 
-// Если всё в порядке, то добавляем запись в БД
-// Подготавливаем SQL запрос, добавляем новое поле `test`
 $query = "INSERT INTO `log` (date_time_event, user_id, group_id, text, error, ok, type, fio, group_name, test) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 if($stmt = $mysqli->prepare($query)) {
-    $testVal = (int) $test; // Преобразуем boolean в int
-    // Привязываем 'date_time_event', 'group_id' и другие параметры к маркерам в подготовленном запросе, включая `test`
-    $stmt->bind_param('siissssssi',
+    // Привязываем параметры к маркерам в подготовленном запросе, используем значения из $data
+    $stmt->bind_param('sisssssssi',
         $currentDateTime,
         $userID,
         $groupID,
@@ -76,9 +66,9 @@ if($stmt = $mysqli->prepare($query)) {
         $error,
         $ok,
         $type,
-        $fio,  //  Тип данных должен быть "i" (целое число)
-        $groupName, //  Тип данных должен быть "s" (строка)
-        $testVal); // Тип данных должен быть "i" (целое число)
+        $fio,
+        $groupName,
+        $test);
 
     // Выполняем запрос
     if ($stmt->execute()) {
@@ -88,14 +78,12 @@ if($stmt = $mysqli->prepare($query)) {
         echo json_encode(['error' => 'Failed to add log entry']);
     }
 
-    // Закрываем запрос
     $stmt->close();
 } else {
     http_response_code(500);
     echo json_encode(['error' => "SQL query preparation failed: " . $mysqli->error]);
 }
 
-// Закрываем соединение
 $mysqli->close();
 
 ?>
