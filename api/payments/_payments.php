e55e4b4a837537cb4fbe937a483bb522b8e424b9
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-06-10 16:56:05 +0300

    Create _payments.php

---
+++ api/payments/_payments.php
@@ -0,0 +1,100 @@
+<?php
+
+// Получаем сегодняшнюю дату
+$today = date("Y-m-d");
+
+// Извлекаем userID из параметров запроса
+$userId = $_GET['user_id'] ?? null;
+
+// Проверяем, был ли предоставлен userID
+if (!$userId) {
+    http_response_code(400);
+    echo json_encode(['error' => 'No user ID provided']);
+    exit;
+}
+
+// Получаем дату из параметров запроса (если она передана)
+$date = $_GET['date'] ?? $today; // По умолчанию используем сегодняшний день
+
+// Подключаем конфигурационный файл
+$dbConfig = require 'sql_config.php';
+
+// Параметры для подключения к БД
+$server = $dbConfig['server'];
+$user = $dbConfig['user'];
+$pass = $dbConfig['pass'];
+$db = $dbConfig['db'];
+
+$mysqli = new mysqli($server, $user, $pass, $db);
+
+if ($mysqli->connect_error) {
+    http_response_code(500);
+    echo json_encode(['error' => "Connection failed: " . $mysqli->connect_error]);
+    exit;
+}
+
+$mysqli->set_charset('utf8mb4');
+
+// Подготовленный запрос для получения данных по userID за указанную дату
+$query = "SELECT 
+    payments.id,
+    payments.date,
+    payments.fio,
+    payments.user_id,
+    payments.operator_type,
+    payments.base,
+    payments.grade,
+    payments.work_hours,
+    payments.tabel_hours,
+    payments.payment,
+    payments.inn,
+    payments.vvp,
+    payments.kpi_good,
+    payments.rating_good,
+    payments.kpi_brak,
+    payments.rating_brak,
+    payments.group_count,
+    payments.color,
+    payments.post,
+    payments.grade_info,
+    payments.smena,
+    (SELECT DISTINCT payments_diff 
+     FROM payment_stats
+     WHERE date = '$date' 
+) AS payments_diff,
+    ps.prod_diff, 
+    ps.rating_pos, 
+    ps.kpi 
+FROM 
+    `payments`
+LEFT JOIN 
+    `payment_stats` ps ON payments.`date` = ps.`date` AND payments.`smena` = ps.`smena` AND payments.`operator_type` = ps.`type`
+WHERE 
+    payments.`user_id` = '$userId' AND payments.`date` = '$date';
+";
+
+if ($stmt = $mysqli->prepare($query)) {
+    $stmt->bind_param('ss', $userId, $date); // Используем userId и date из GET
+
+    if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        $payments = [];
+        while ($row = $result->fetch_assoc()) {
+            $payments[] = $row;
+        }
+
+        $stmt->close();
+        $mysqli->close();
+
+        echo json_encode(['payments' => $payments]);
+    } else {
+        http_response_code(500);
+        echo json_encode(['error' => "SQL query execution failed: " . $mysqli->error]);
+        exit;
+    }
+} else {
+    http_response_code(500);
+    echo json_encode(['error' => 'SQL query preparation failed: ' . $mysqli->error]);
+}
+
+?>
